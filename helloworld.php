9df19bb60cf79d51a9df25048df42ec1ffa45d37
jannie <684259021@webmail.npru.ac.th>
2026-02-27 11:58:04 +0700

    Add files via upload

---
+++ helloworld.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arisa</title>
+</head>
+
+<body>
+    Hello World of php><
+        </body>
+
+</html>
